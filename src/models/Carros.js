e3a8302b8e617b0ff4dc22fa2442d6d73ed6b914
Lucasnnn <olucas58@gmail.com>
2022-11-20 19:09:30 -0300

    att

--- src/models/Carros.js
+++ src/models/Carros.js
@@ -4,8 +4,8 @@ const CarroSchema = new mongooose.Schema({
   linkFoto: { type: String, required: true },
   nome: { type: String, required: true },
   tipo: { type: String, required: true },
-  ano: { type: string, required: true },
-  potencia: { type: string, required: true },
+  ano: { type: String, required: true },
+  potencia: { type: String, required: true },
 });
 
 const Carro = mongooose.model('carros', CarroSchema);
